11bd542d427d674e732c99391c5c38d9f2f8d715
stdavis <stdavis@utah.gov>
2025-06-03 09:26:52 -0600

    deps: bump palletjack to v5

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     },
     keywords=["gis"],
     install_requires=[
-        "ugrc-palletjack>=4.1,<5",
+        "ugrc-palletjack>=5.2.1",
         "ugrc-supervisor>=3.1.3",
     ],
     extras_require={
